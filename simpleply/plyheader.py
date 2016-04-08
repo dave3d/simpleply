59609f1e5da539ea1131ce9a0a46244787901d3b
David T. Chen <dchen@mail.nih.gov>
2016-04-08 16:01:05 -0400

    added vertex normal

--- simpleply/plyheader.py
+++ simpleply/plyheader.py
@@ -5,26 +5,39 @@ from __future__ import print_function
 import sys
 
 
-def plyheader(nverts, nfaces, vcolorFlag=False, tcoordFlag=False, fout=sys.stdout):
+def plyheader(nverts, nedges, nfaces, vcolorFlag=False, vnormFlag=False, tcoordFlag=False, fout=sys.stdout):
 
-  print ("ply", file=fout)
-  print ("format ascii 1.0", file=fout)
-  print ("element vertex", nverts, file=fout)
-  print ("property float x", file=fout)
-  print ("property float y", file=fout)
-  print ("property float z", file=fout)
+    print ("ply", file=fout)
+    print ("format ascii 1.0", file=fout)
 
-  if vcolorFlag:
-    print ("property uchar red", file=fout)
-    print ("property uchar green", file=fout)
-    print ("property uchar blue", file=fout)
+    if nverts>0:
+        print ("element vertex", nverts, file=fout)
+        print ("property float x", file=fout)
+        print ("property float y", file=fout)
+        print ("property float z", file=fout)
 
-  if tcoordFlag:
-    print ("property float u", file=fout)
-    print ("property float v", file=fout)
+        if vcolorFlag:
+            print ("property uchar red", file=fout)
+            print ("property uchar green", file=fout)
+            print ("property uchar blue", file=fout)
 
-  print ("element face", nfaces, file=fout)
-  print ("property list uchar int vertex_indices", file=fout)
+        if vnormFlag:
+            print ("property float nx", file=fout)
+            print ("property float ny", file=fout)
+            print ("property float nz", file=fout)
 
-  print ("end_header", file=fout)
+        if tcoordFlag:
+            print ("property float u", file=fout)
+            print ("property float v", file=fout)
+
+    if nedges>0:
+        print ("element face", nfaces, file=fout)
+        print ("property uint e0", file=fout)
+        print ("property uint e1", file=fout)
+
+    if nfaces>0:
+        print ("element face", nfaces, file=fout)
+        print ("property list uchar int vertex_indices", file=fout)
+
+    print ("end_header", file=fout)
 
