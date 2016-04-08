ecbb9581c71eaff42a48480b3afffaa256d0b35d
David T. Chen <dchen@mail.nih.gov>
2016-04-08 14:29:41 -0400

    Initial checkin of setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='simpleply',
+      version='0.1',
+      description='A simple PLY file module',
+      url='http://github.com/dave3d/simpleply',
+      author='Dave Chen',
+      author_email='dchen@mail.nih.gov',
+      license='MIT',
+      packages=['simpleply'],
+      zip_safe=False)
