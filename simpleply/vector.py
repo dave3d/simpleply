75b90f3114cdac2fcaec6720d9a48563969345da
David T. Chen <dchen@mail.nih.gov>
2016-04-12 11:41:06 -0400

    Add distance function and docstrings

--- simpleply/vector.py
+++ simpleply/vector.py
@@ -6,24 +6,28 @@
 import math
 
 def add(a,b):
+    """Returns the sum of two vectors"""
     c = []
     for ax, by in zip(a,b):
         c.append(ax+by)
     return c
 
 def subtract(a,b):
+    """Returns the difference of two vectors"""
     c = []
     for ax, by in zip(a,b):
         c.append(ax-by)
     return c
 
 def dot(a,b):
+    """Returns the dot product of two vectors"""
     c = []
     for ax, by in zip(a,b):
         c.append(ax*by)
     return c
 
 def cross(a,b):
+    """Returns the cross product of two vectors"""
     c = [0,0,0]
     c[0] = a[1]*b[2] - b[1]*a[2]
     c[1] = -(a[0]*b[2] - b[0]*a[2])
@@ -31,12 +35,14 @@ def cross(a,b):
     return c
 
 def length(a):
+    """Returns the length of a vector"""
     s = 0.0
     for x in a:
         s = s + x*x
     return math.sqrt(s)
 
 def normalize(a):
+    """Returns a normalized version of a vector"""
     l = length(a)
     c = []
     if l == 0.0:
@@ -44,6 +50,11 @@ def normalize(a):
     for x in a:
         c.append(x/l)
     return c
+
+def distance(a,b):
+    """Returns the distance between two vectors"""
+    c = subtract(a,b)
+    return length(c)
 
 if __name__ == "__main__":
 
@@ -60,3 +71,5 @@ if __name__ == "__main__":
 
     print "length(v1):", length(v1)
     print "normalize(v1):", normalize(v1)
+
+    print "distance(v1,v2):", distance(v1,v2)
